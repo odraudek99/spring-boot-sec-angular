08ff3531f10defd43ca9b8ded004c86ab6a8bc67
odraudek99 <Krayola91>
2015-08-04 10:52:29 -0500

    Corrección de problemas de security - angular

--- angular/src/main/resources/public/js/EditController.js
+++ angular/src/main/resources/public/js/EditController.js
@@ -1,43 +1,21 @@
 
-msmApp.register.controller('EditController', 	function EditController($scope, $location, NewsService, $routeParams, $http, $cookieStore) {
+msmApp.register.controller('EditController', 	function EditController($scope, $location, NewsService, $routeParams, $http, $cookieStore, $resource) {
 
+	$scope.newsEntry = $resource('http://localhost:8080/news/:id', 
+			{
+				id: '@id'
+			})
+			.get(
+			{
+				id: $routeParams.id
+			});
 
-	$scope.getNewsEntrie = function($routeParams) {
-		
-		var user = $cookieStore.get('user');
-	
-		
-		$http.defaults.headers.common[ 'x-auth-token' ] = user.token;
-		$cookieStore.put('user', user);
-		
-	
-		$http({
-	        method: 'GET',
-	        url: 'http://localhost:8080/news/'+$routeParams.id,
-	        
-	        headers: {
-	            'x-auth-token': user.token
-	        }
-	    }).success(function(data, status, headers, config) {
-	    	$scope.newsEntry = data;
-	    	
-	    	console.info("data: " + data);
-	    	
-	    }).error(function(data, status, headers, config) {
-	        if (status === 400) {
-	            defered.reject(data);
-	        } else {
-	            throw new Error("Fallo obtener los datos:" + status + "\n" + data);
-	        }
-	    });
-    }
-	$scope.getNewsEntrie($routeParams);
-	
-	
 	$scope.save = function() {
 		$scope.newsEntry.$save(function() {
 			$location.path('/');
 		});
 	};
+
+
 	
 });
--- x-auth-security/src/main/java/demo/SimpleCORSFilter.java
+++ x-auth-security/src/main/java/demo/SimpleCORSFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
@@ -15,14 +16,24 @@ import org.springframework.stereotype.Component;
 public class SimpleCORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		
 		HttpServletResponse response = (HttpServletResponse) res;
+		
 		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Content-Type,x-auth-token,Cookie");
 		
+		HttpServletRequest request = (HttpServletRequest) req;
 		
-		chain.doFilter(req, res);
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+	        response.setStatus(HttpServletResponse.SC_OK);
+	    } else {
+	        chain.doFilter(req, res);
+	    }
+		
+		
 	}
 
 	public void init(FilterConfig filterConfig) {}
